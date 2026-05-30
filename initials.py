3b375ebf358afbdba321b5c90b31ce65222a39f7
Fiifi-GCTU <2425401430@live.gctu.edu.gh>
2026-05-30 23:34:21 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 2
+#
+# Name: Fiifi Kwakye Koomson
+# STUDENT ID: 2425401430
+# Email: 2425401430@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+#On my honor, Fiifi Kwakye Koomson, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
-
+    # Printing out stylized initials of my name using the print function and Strings.
+    print()
+    print("...FKK")
+    print()
+    print("...FFFFFFFFFFFF........KKKK....KKKK........KKKK....KKKK.....")
+    print("...FFFFFFFFFFFF........KKKK...KKKK.........KKKK...KKKK......")
+    print("...FFFF................KKKK..KKKK..........KKKK..KKKK.......")
+    print("...FFFF................KKKK.KKKK...........KKKK.KKKK........")
+    print("...FFFFFFFFF...........KKKKKKKK............KKKKKKKK.........")
+    print("...FFFFFFFFF...........KKKKKKKK............KKKKKKKK.........")
+    print("...FFFF................KKKK.KKKK...........KKKK.KKKK........")
+    print("...FFFF................KKKK..KKKK..........KKKK..KKKK.......")
+    print("...FFFF...........**...KKKK...KKKK....**...KKKK...KKKK....**")
+    print("...FFFF...........**...KKKK....KKKK...**...KKKK....KKKK...**")
+    print()
 main()
+
+
